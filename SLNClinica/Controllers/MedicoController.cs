b788690f3e23f0d7b8cf094f4bea47eee915cccf
IannielloR <rianniello@fi.uba.ar>
2023-01-27 16:52:44 -0300

    controller

--- SLNClinica/Controllers/MedicoController.cs
+++ SLNClinica/Controllers/MedicoController.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SLNClinica.Data;
+using SLNClinica.Models;
 
 namespace SLNClinica.Controllers
 {
@@ -15,6 +17,26 @@ namespace SLNClinica.Controllers
         public IActionResult Index()
         {
             var medico = context.Medicos.ToList();
+            return View(medico);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Medico medico = new Medico();
+
+            return View("Create", medico);
+        }
+
+        //post: Opera/Create
+        [HttpPost]
+        public ActionResult Create(Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Medicos.Add(medico);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View(medico);
         }
     }
